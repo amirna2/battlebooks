47bf7abd25b1f79bb2d6a339904aed2ae62f54e6
amirna2 <=amir.nth@gmail.com>
2019-11-24 17:50:42 -0800

    Refactored book handler to use a book service component instead of using the repository directly

--- src/main/java/com/example/battlebooks/handler/BookHandler.java
+++ src/main/java/com/example/battlebooks/handler/BookHandler.java
@@ -2,8 +2,6 @@ package com.example.battlebooks.handler;
 
 import static reactor.core.publisher.Mono.error;
 
-import java.util.Optional;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +14,9 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.server.ResponseStatusException;
 import com.example.battlebooks.model.Book;
 import com.example.battlebooks.repository.BookRepository;
+import com.example.battlebooks.service.BookService;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -24,7 +24,6 @@ public class BookHandler {
 
 	public static final String  API_BOOKS = "/api/books";
     final Logger logger = LogManager.getLogger(BookHandler.class.getSimpleName());
-
     
 	static final Mono<ServerResponse> notFound = ServerResponse.notFound().build();
 	static final Mono<ServerResponse> badRequest = ServerResponse.badRequest().build();
@@ -33,20 +32,23 @@ public class BookHandler {
 	static final Mono<ServerResponse> serverError = ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 
 	@Autowired
-	BookRepository repository;
+	BookService bookService;
 	
     public Mono<ServerResponse> getAllBooks(ServerRequest request) {
     	
     	logger.info("getAllBooks: request {}",request.toString());
-        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(repository.findAll(), Book.class);
+    	
+    	Flux<Book> books = bookService.getAllBooks();
+    	
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(books, Book.class);
     }
 
     public Mono<ServerResponse> getBookById(ServerRequest request) {
         String id = request.pathVariable("id");
         
-        Mono<Book> found = repository.findById(id);
+        Mono<Book> foundBook = bookService.getBookById(id);
         
-        return found.flatMap(book -> ServerResponse.ok()
+        return foundBook.flatMap(book -> ServerResponse.ok()
                         .contentType(MediaType.APPLICATION_JSON)
                         .body(BodyInserters.fromValue(book)))
                     .switchIfEmpty(notFound);
@@ -54,36 +56,53 @@ public class BookHandler {
         
     public Mono<ServerResponse> createBook(ServerRequest request) {
         Mono<Book> bookToCreate = request.bodyToMono(Book.class);
-        return bookToCreate.flatMap(item -> 
+        
+        return bookToCreate.flatMap(book -> 
             ServerResponse.created(null)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(repository.save(item),Book.class));
+                .body(bookService.saveBook(book),Book.class));
     }
    
     public Mono<ServerResponse> deleteBook(ServerRequest request) {
     	String id = request.pathVariable("id");        
 
-		return repository.findById(id)
+		return bookService.getBookById(id)
 			.switchIfEmpty(error(new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED)))
 			.flatMap(book -> {
-				return repository.deleteById(id).then(Mono.just(book));
+				return bookService.deleteBookById(id).then(Mono.just(book));
 			})
-			.flatMap((deletedCustomer) -> { return noContent;});
+			.flatMap(deletedBook -> noContent);
 	}
     
     public Mono<ServerResponse> updateBook(ServerRequest request) {
-	    String id = request.pathVariable("id");
-	    
+	    String id = request.pathVariable("id");	    
 	    final String notFoundReason = "Book ID "+id+" does not exist";
 	    
-	    return repository.findById(id)
+	    Mono<Boolean> exists = bookService.bookExists(id);
+
+	    /*	    
+	    return bookService.getBookById(id)
 	    		  .switchIfEmpty(error(new ResponseStatusException(HttpStatus.NOT_FOUND, notFoundReason)))
 	    		  .then(request.bodyToMono(Book.class))
-	    		  .flatMap( update -> repository.save(update))
+	    		  .flatMap( update -> bookService.saveBook(update))
 	    		  .flatMap(saved -> ServerResponse.ok()
 						.contentType(MediaType.APPLICATION_JSON)
 						.body(BodyInserters.fromValue(saved)))
 				  .switchIfEmpty(serverError);
-	    		  
-    }
+	     */
+
+	    return exists.
+	    		flatMap(bookExists -> {
+	    			if( !bookExists) {
+	    				return error(new ResponseStatusException(HttpStatus.NOT_FOUND, notFoundReason));
+	    			}
+		    		return request.bodyToMono(Book.class)
+		    			.flatMap( update -> bookService.saveBook(update))
+		    			.flatMap(saved -> ServerResponse.ok()
+							.contentType(MediaType.APPLICATION_JSON)
+							.body(BodyInserters.fromValue(saved)))
+		    			.switchIfEmpty(serverError);
+		    	
+	    	});
+    } 	
 }
---
+++ src/main/java/com/example/battlebooks/service/BookService.java
@@ -0,0 +1,39 @@
+package com.example.battlebooks.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.battlebooks.model.Book;
+import com.example.battlebooks.repository.BookRepository;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class BookService {
+	
+	private BookRepository bookRepo;
+	
+	public BookService(BookRepository bookRepo) {
+		this.bookRepo = bookRepo;		
+	}
+
+	public Flux<Book> getAllBooks() {
+		return bookRepo.findAll();
+	}
+	
+	public Mono<Book> getBookById(String id) {
+		return bookRepo.findById(id);
+	}
+	
+	public Mono<Void> deleteBookById(String id) {
+		return bookRepo.deleteById(id);
+	}
+	
+	public Mono<Book> saveBook(Book book) {
+		return bookRepo.save(book);
+	}
+	
+	public Mono<Boolean> bookExists(String id) {
+		return bookRepo.existsById(id);
+	}
+}
